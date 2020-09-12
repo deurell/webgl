d2e3480646d72d9aa2c4becc1770d5a1c69fc6d1
Mikael Deurell <mikael@deurell.net>
2020-09-12 13:26:53 +0200

    fot the love of const...

--- planet_writer/main.js
+++ planet_writer/main.js
@@ -1,6 +1,5 @@
 import * as THREE from '../node_modules/three/build/three.module.js';
 
-
 const MAX_POINTS = 1024 * 4;
 
 function getRandomInt(min, max) {
@@ -9,7 +8,7 @@ function getRandomInt(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-var render = function(time) {
+const render = function(time) {
     requestAnimationFrame(render);
 
     if (points_drawn >= MAX_POINTS) {
@@ -17,10 +16,10 @@ var render = function(time) {
     }
 
     if (points_drawn % 1 == 0) {
-        var height = 4;
-        var geometry = new THREE.BoxGeometry(0.2, 0.2, height);
-        var material = new THREE.MeshBasicMaterial({ color: 0xffffff });
-        var cube = new THREE.Mesh(geometry, material);
+        const height = 4;
+        const geometry = new THREE.BoxGeometry(0.2, 0.2, height);
+        const material = new THREE.MeshBasicMaterial({ color: 0xffffff });
+        const cube = new THREE.Mesh(geometry, material);
         cube.position.copy(points[points_drawn].clone().applyMatrix4(line.matrixWorld));
         cube.lookAt(new THREE.Vector3(0, 0, 0));
         cubes.push(cube);
@@ -33,9 +32,9 @@ var render = function(time) {
     }
 
     for (let i = 0; i < cubes.length; i++) {
-        var pos = cubes[i].position;
+        const pos = cubes[i].position;
         cubes[i].position.copy(pos.multiplyScalar(1.04));
-        var col = cubes[i].material.color;
+        const col = cubes[i].material.color;
         cubes[i].material.color.copy(cubes[i].material.color.multiplyScalar(0.97));
     }
 
@@ -48,33 +47,33 @@ var render = function(time) {
     renderer.render(scene, camera);
 };
 
-var cubes = [];
-var renderer = new THREE.WebGLRenderer();
+let cubes = [];
+const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
-var camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 500);
+const camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 500);
 camera.position.set(0, 0, 200);
 camera.lookAt(0, 0, 0);
 
-var clock = new THREE.Clock();
+const clock = new THREE.Clock();
 
-var scene = new THREE.Scene();
-var material = new THREE.LineBasicMaterial({ color: 0xffffff });
-var points = [];
-var points_drawn = 0;
+const scene = new THREE.Scene();
+const material = new THREE.LineBasicMaterial({ color: 0xffffff });
+const points = [];
+let points_drawn = 0;
 
-var radius = 20;
-var xa = 0;
-var ya = 0;
-var dist = 0.22;
+const radius = 20;
+let xa = 0;
+let ya = 0;
+let dist = 0.22;
 
 for (let i = 0; i < MAX_POINTS; i++) {
-    let scale = 0.12;
-    let x = radius * Math.sin(ya) * Math.cos(xa);
-    let y = radius * Math.sin(ya) * Math.sin(xa);
-    let z = radius * Math.cos(ya);
-    let point = new THREE.Vector3(x, y, z);
+    const scale = 0.12;
+    const x = radius * Math.sin(ya) * Math.cos(xa);
+    const y = radius * Math.sin(ya) * Math.sin(xa);
+    const z = radius * Math.cos(ya);
+    const point = new THREE.Vector3(x, y, z);
     points.push(point);
     let res = Math.random();
     if (res < 0.1) {
@@ -88,8 +87,8 @@ for (let i = 0; i < MAX_POINTS; i++) {
     }
 }
 
-var geometry = new THREE.BufferGeometry().setFromPoints(points);
-var line = new THREE.Line(geometry, material);
+const geometry = new THREE.BufferGeometry().setFromPoints(points);
+const line = new THREE.Line(geometry, material);
 scene.add(line);
 
 render();
